7b59048f7b7462b00d983accc03376166707e141
Niq256 <twesigyeniclous@gmail.com>
2025-03-07 13:38:52 +0300

    core requirements 1 & 2

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Console.WriteLine("Please enter your grade percentage:");
+        string score = Console.ReadLine();
+        int x = int.Parse(score);
+        if (x >= 90)
+        {
+        Console.WriteLine("You scored grade A");
+        }
+        else if ((x < 90) && (x >= 80)) 
+        {
+        Console.WriteLine("You scored grade B");
+        }
+        else if ((x < 80) && (x >= 70)) 
+        {
+        Console.WriteLine("You scored grade C");
+        }
+        else if ((x < 70) && (x >= 60)) 
+        {
+        Console.WriteLine("You scored grade D");
+        }
+        else 
+        Console.WriteLine("You scored grade F");
+        
+        if (x >= 70)
+        {
+            Console.WriteLine("Congradulations! you passed.");
+        }
+        else 
+        Console.WriteLine("Unfortunately you failed. Please try again next time.");
+
     }
 }
