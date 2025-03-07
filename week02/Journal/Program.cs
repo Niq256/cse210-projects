d1d4b45cbc602422a9d1ad92d3b15807ec919d87
Niq256 <twesigyeniclous@gmail.com>
2025-03-07 15:31:05 +0300

    edited by mistake

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,61 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Please enter your grade percentage:");
-        string score = Console.ReadLine();
-        int x = int.Parse(score);
-
-        string grade = "";
-        string sign = "";
-
-        if (x >= 90)
-        {
-        grade = "A";
-        }
-        else if ((x < 90) && (x >= 80)) 
-        {
-        grade = "B";
-        }
-        else if ((x < 80) && (x >= 70)) 
-        {
-        grade = "C";
-        }
-        else if ((x < 70) && (x >= 60)) 
-        {
-        grade = "D";
-        }
-        else 
-        {
-        grade = "F";
-        }
-        
-        if (grade != "A" && grade != "F")
-        {
-            int lastDigit = x % 10;
-
-            if (lastDigit >= 7)
-            {
-                sign = "+";
-            }
-            else if (lastDigit < 3)
-            {
-                sign = "-";
-            }
-            else
-            {
-                sign = "";
-            }
-        }
-
-        Console.WriteLine($"You scored grade {grade}{sign}");
-
-        if (x >= 70)
-        {
-            Console.WriteLine("Congratulations! You passed.");
-        }
-        else
-        {
-            Console.WriteLine("Unfortunately, you failed. Please try again next time.");
-        }
+        Console.WriteLine("Hello World!.");
     }
 }
